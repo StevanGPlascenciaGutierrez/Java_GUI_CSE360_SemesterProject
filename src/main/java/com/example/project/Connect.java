1601497f71d8b98cb49a1b9ee308c62c409d28fa
StevanGPlascenciaGutierrez <83990683+StevanGPlascenciaGutierrez@users.noreply.github.com>
2021-11-12 14:04:55 -0700

    Made some changes to the database, added methods to Connect.java

--- src/main/java/com/example/project/Connect.java
+++ src/main/java/com/example/project/Connect.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.sql.ResultSet;
+import java.util.Locale;
 
 
 public class Connect {
@@ -22,11 +25,67 @@ public class Connect {
         return null;
     }
 
-    public void Execute(String stmt){//Executes SQLite Statements
-        try(Statement sql = conn.createStatement()){
-            sql.execute(stmt);
+    public int loginPatient(String user, String pass) {
+        String sql = "SELECT email, password, patientID FROM Patient WHERE email = ?";
+
+        try (Connection conn = Connect()) {
+            user = user.toLowerCase();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1,user);
+            ResultSet rs = stmt.executeQuery(sql);
+            String email = rs.getString("email");
+            String password = rs.getString("password");
+            int patientID = rs.getInt("patientID");
+            if(user == email){
+                if(pass == password){
+                    return patientID;
+                }
+            }
+            return -1;
+        } catch (SQLException e) {
+            return -1;
+        }
+    }
+
+    public int loginStaff(int id, String pass){
+        String sql = "SELECT doctorID, password FROM Patient WHERE doctorID = ?";
+
+        try (Connection conn = Connect()) {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1,id);
+            ResultSet rs = stmt.executeQuery(sql);
+            int doctorID = rs.getInt("doctorID");
+            String password = rs.getString("password");
+            if(id == doctorID){
+                if(pass == password){
+                    return doctorID;
+                }
+            }
+            return -1;
+        } catch (SQLException e) {
+            return -1;
+        }
+    }
+
+    public boolean signUp(String fName, String lName, String bDay, String email, String pass){
+        try(Connection conn = Connect()){
+            String sql = "INSERT INTO Patient(name, birthday, emailAddress, password) VALUES (?,?,?,?)";
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1,fName+lName);
+            stmt.setString(2, bDay);
+            stmt.setString(3,email);
+            stmt.setString(4,pass);
+            return true;
         }catch(SQLException e){
-            System.out.println(e.getMessage());
+          return false;
+        }
+    }
+
+    public void close(Connection conn){
+        try {
+            conn.close();
+        }catch(SQLException e){
+            return;
         }
     }
 }
