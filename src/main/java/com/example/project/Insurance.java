0807f4951741f7b762fc7fb8876145a2899018cf
SashRajj <91229376+SashRajj@users.noreply.github.com>
2021-11-15 13:16:46 -0700

    Update Insurance.java

--- src/main/java/com/example/project/Insurance.java
+++ src/main/java/com/example/project/Insurance.java
@@ -1,5 +1,9 @@
 package com.example.project;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Insurance {
     private String name;
     private int phoneNumber;
@@ -26,5 +30,17 @@ public class Insurance {
 
     public void setPhoneNumber(Integer phoneNumber){
         this.phoneNumber = phoneNumber;
+    }
+    
+    public static void insert(String Name, int PhoneNumber){
+        String sql = "INSERT INTO PATIENT(name, phoneNumber) VALUES(?,?)";
+        try (Connection conn = Connect.conn;
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, Name);
+            pstmt.setInt(2, PhoneNumber);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
