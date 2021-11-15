d4abbfe2ddc70fcc2ed70c853902fea7cab4ba6e
StevanGPlascenciaGutierrez <83990683+StevanGPlascenciaGutierrez@users.noreply.github.com>
2021-11-14 18:27:03 -0700

    Added Select Methods to DoctorDashboard.java and PatientDashboard.java

--- src/main/java/com/example/project/PatientDashboard.java
+++ src/main/java/com/example/project/PatientDashboard.java
@@ -1,5 +1,9 @@
 package com.example.project;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class PatientDashboard extends Dashboard {
@@ -34,5 +38,48 @@ public class PatientDashboard extends Dashboard {
     }
     public void setImmunizations(ArrayList<Immunization> newImmunizations) {
         immunizations = newImmunizations;
+    }
+
+    //Returns Patient Dashboard object from SQLite Database
+    public PatientDashboard select(int patientID){
+
+        //SQLite Queries
+        String insSQL = "SELECT * FROM Insurance WHERE patientID = ? ";
+        String phaSQL = "SELECT * FROM Pharmacy WHERE patientID = ?";
+        String immSQL = "SELECT * FROM Immunizations WHERE patientID = ?";
+
+        //Array List to hold immunizations
+        immunizations = new ArrayList<Immunization>();
+
+        //Establish Connection
+        try (Connection conn = Connect.conn ) {
+
+            //Gets Insurance from database
+            PreparedStatement pstmt = conn.prepareStatement(insSQL);
+            pstmt.setInt(1, patientID);
+            ResultSet rs = pstmt.executeQuery();
+            insurance = new Insurance(rs.getString("name"),rs.getInt("phoneNumber"));
+
+
+            //Gets Pharmacy from database
+            pstmt = conn.prepareStatement(phaSQL);
+            pstmt.setInt(1, patientID);
+            rs = pstmt.executeQuery();
+            pharmacy = new Pharmacy(rs.getString("name"),rs.getString("address"),rs.getInt("phoneNumber"));
+
+
+            //Gets Immunizations from database
+            pstmt = conn.prepareStatement(immSQL);
+            pstmt.setInt(1,patientID);
+            rs = pstmt.executeQuery();
+            while(rs.next()){
+                Immunization imm = new Immunization(rs.getString("type"), rs.getString("date"), rs.getString("description"));
+                immunizations.add(imm);
+            }
+
+            return this;
+        }catch(SQLException e){
+            return null;
+        }
     }
 }
